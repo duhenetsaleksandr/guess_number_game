6e00e1958dbc78dd56d1dc536c410b3344896c91
aleksandrkonstantinov <Dedul1A@ukr.net>
2021-11-05 13:08:52 +0200

    initial values

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
-function randomInteger(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+const inputUserValue = document.getElementById("user_number");
+const checkBtn = document.getElementById("check");
+const newGameBtn = document.getElementById("new_game");
+
+const mainText = document.getElementById("main_text");
+const promptText = document.getElementById("prompt_text");
+const shotsLeft = document.getElementById("shots");
+
+const initialValues = {};
+
+function start(min = 1, max = 100, shots = 5) {
+	initialValues.min = min;
+	initialValues.max = max;
+	initialValues.shots = shots;
+	initialValues.mysNumbers = randomInteger(min, max);
 }
+
+function randomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+start();
