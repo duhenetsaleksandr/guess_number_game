3814a0058dffe65d93ce353b01fa25c340389921
aleksandrkonstantinov <Dedul1A@ukr.net>
2021-11-12 13:10:50 +0200

    fixed style

--- scripts/game-states.js
+++ scripts/game-states.js
@@ -1,20 +1,23 @@
 function start(min = 1, max = 100, shots = 5) {
-   initialValues.min = min;
-   initialValues.max = max;
-   initialValues.shots = shots;
-   initialValues.shotsCounter = shots;
-   initialValues.mysNumbers = randomInteger(min, max);
+	initialValues.min = min;
+	initialValues.max = max;
+	initialValues.shots = shots;
+	initialValues.shotsCounter = shots;
+	initialValues.mysNumbers = randomInteger(min, max);
 
-   checkBtn.removeAttribute('disabled');
-   show(MAIN_TEXT_MESSAGE, PROMPT_TEXT_MESSAGE);
-   inputUserValue.value = '';
+	checkBtn.removeAttribute('disabled');
+	inputUserValue.removeAttribute('disabled');
+	show(MAIN_TEXT_MESSAGE, PROMPT_TEXT_MESSAGE);
+	inputUserValue.value = '';
+	inputUserValue.focus();
 }
 
 function newGame(event) {
-   event.preventDefault();
-   start();
+	event.preventDefault();
+	start();
 }
 
 function stop() {
-   checkBtn.setAttribute('disabled', 'disabled');
+	checkBtn.setAttribute('disabled', 'disabled');
+	inputUserValue.setAttribute('disabled', 'disabled');
 }
