46d2329a05f06f888329c95e9ddfec1cccd5b110
duhenetsaleksandr <duhenets@gmail.com>
2021-11-16 22:47:56 +0200

    restructuring js files

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,32 @@
+function init(min = 1, max = 100, shots = 5) {
+	initialValues.min = min;
+	initialValues.max = max;
+	initialValues.shots = shots;
+	initialValues.shotsCounter = shots;
+	initialValues.mysNumbers = randomInteger(min, max);
+
+	checkBtn.removeAttribute('disabled');
+	inputUserValue.removeAttribute('disabled');
+	show(MAIN_TEXT_MESSAGE, PROMPT_TEXT_MESSAGE);
+	inputUserValue.value = '';
+	inputUserValue.focus();
+}
+
+function newGame(event) {
+	event.preventDefault();
+	init();
+}
+
+function stop() {
+	checkBtn.setAttribute('disabled', 'disabled');
+	inputUserValue.setAttribute('disabled', 'disabled');
+}
+
 checkBtn.addEventListener('click', generate);
 newGameBtn.addEventListener('click', newGame);
 inputUserValue.addEventListener('keydown', enterSubmit);
 
 window.onload = function() {
-	start();
+	init();
 };
 
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,3 +1,7 @@
+function randomInteger(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function show(title, prompt, shots = `${initialValues.shotsCounter} shots left`){
    mainText.innerText = title;
    promptText.innerText = prompt;
