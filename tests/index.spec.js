0b1b4561f77462945655f9476ba933fe70bb968e
duhenetsaleksandr <duhenets@gmail.com>
2021-11-18 19:20:51 +0200

    added tests for settings

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -48,6 +48,19 @@ describe('On HTML', function () {
       expect(document.getElementById('main_text').innerText.toLowerCase()).to.be.equal('you win');
    });
 
+   it('should show message with amount of attempts needed if user "YOU WIN"', function () {
+      window.onload = () => {}
+      init();
+      const number = faker.random.number(initialValues.shots - 1);
+      for (let i = 0; i < number; i++) {
+         document.getElementById('user_number').value = initialValues.mysNumbers - 1;
+         document.getElementById('check').click();
+      }
+      document.getElementById('user_number').value = initialValues.mysNumbers;
+      document.getElementById('check').click();
+      expect(document.getElementById('shots').innerText).to.be.equal(`you needed ${number + 1} shots`);
+   });
+
    it('should show message "NUMBER OUTSIDE THE RANGE" if number greater than max range', function () {
       window.onload = () => {}
       init();
@@ -118,5 +131,89 @@ describe('On HTML', function () {
       init();
       expect(document.getElementById('main_text').innerText.toLowerCase()).to.be.equal('try your luck');
       expect(parseInt(document.getElementById('shots').innerText)).to.be.equal(initialValues.shots);
+   });
+
+   it('should change range and amount attempts', function () {
+      window.onload = () => {}
+      init();
+      document.getElementById('min-sett').value = 10;
+      document.getElementById('max-sett').value = 50;
+      document.getElementById('shots-sett').value = 10;
+      document.getElementById('config').click();
+
+      expect(parseInt(document.getElementById('min-info').innerText)).to.be.equal(10);
+      expect(parseInt(document.getElementById('max-info').innerText)).to.be.equal(50);
+      expect(parseInt(document.getElementById('shots').innerText)).to.be.equal(10);
+   });
+
+   it('shouldn`t change range and amount attempts if min is empty string', function () {
+      window.onload = () => {}
+      init();
+      document.getElementById('min-sett').value = '';
+      document.getElementById('max-sett').value = 60;
+      document.getElementById('shots-sett').value = 8;
+      document.getElementById('config').click();
+
+      expect(parseInt(document.getElementById('min-info').innerText)).to.be.equal(1);
+      expect(parseInt(document.getElementById('max-info').innerText)).to.be.equal(100);
+      expect(parseInt(document.getElementById('shots').innerText)).to.be.equal(5);
+   });
+
+   it('shouldn`t change range and amount attempts if max is empty string', function () {
+      window.onload = () => {}
+      init();
+      document.getElementById('min-sett').value = 10;
+      document.getElementById('max-sett').value = '';
+      document.getElementById('shots-sett').value = 8;
+      document.getElementById('config').click();
+
+      expect(parseInt(document.getElementById('min-info').innerText)).to.be.equal(1);
+      expect(parseInt(document.getElementById('max-info').innerText)).to.be.equal(100);
+      expect(parseInt(document.getElementById('shots').innerText)).to.be.equal(5);
+   });
+
+   it('shouldn`t change range and amount attempts if shots is empty string', function () {
+      window.onload = () => {}
+      init();
+      document.getElementById('min-sett').value = 10;
+      document.getElementById('max-sett').value = 50;
+      document.getElementById('shots-sett').value = '';
+      document.getElementById('config').click();
+
+      expect(parseInt(document.getElementById('min-info').innerText)).to.be.equal(1);
+      expect(parseInt(document.getElementById('max-info').innerText)).to.be.equal(100);
+      expect(parseInt(document.getElementById('shots').innerText)).to.be.equal(5);
+   });
+
+   it('shouldn`t change range and amount attempts if all settings fields is empty string', function () {
+      window.onload = () => {}
+      init();
+      document.getElementById('min-sett').value = '';
+      document.getElementById('max-sett').value = '';
+      document.getElementById('shots-sett').value = '';
+      document.getElementById('config').click();
+
+      expect(parseInt(document.getElementById('min-info').innerText)).to.be.equal(1);
+      expect(parseInt(document.getElementById('max-info').innerText)).to.be.equal(100);
+      expect(parseInt(document.getElementById('shots').innerText)).to.be.equal(5);
+   });
+
+   it('shouldn`t change range and amount attempts if any field is string with spaces', function () {
+      window.onload = () => {}
+      init();
+      document.getElementById('min-sett').value = '   ';
+      document.getElementById('max-sett').value = 50;
+      document.getElementById('shots-sett').value = '   ';
+      document.getElementById('config').click();
+
+      expect(parseInt(document.getElementById('min-info').innerText)).to.be.equal(1);
+      expect(parseInt(document.getElementById('max-info').innerText)).to.be.equal(100);
+      expect(parseInt(document.getElementById('shots').innerText)).to.be.equal(5);
+   });
+
+   it('should change theme to dark-mode', function () {
+      init();
+      document.getElementById('theme_css').click();
+      expect(window.getComputedStyle(document.querySelector('body')).backgroundColor).to.be.equal('rgb(35, 36, 42)');
    });
 });
